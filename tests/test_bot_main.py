c082305b846219df0d1636d97d6dfdec16fc7bc1
alinavoronina <alya.voronina.0600@mail.ru>
2025-05-03 14:46:07 +0300

    Добавила недостающий код в fixtures.py и исправила test_bot_main.py

--- tests/test_bot_main.py
+++ tests/test_bot_main.py
@@ -1,9 +1,9 @@
 import pytest
 from main import main
-from fixtures import mock_bot, mock_dispatcher, mock_set_my_commands, mock_handlers_router, mock_callbacks_router, mock_setup_logger
+from fixtures import mock_bot, mock_dispatcher, mock_set_my_commands, mock_handlers_router, mock_callbacks_router, mock_router, mock_setup_logger
 
 @pytest.mark.asyncio
-async def test_main(mock_bot, mock_dispatcher, mock_setup_logger, mock_set_my_commands):
+async def test_main(mock_bot, mock_dispatcher, mock_setup_logger, mock_set_my_commands, mock_router, mock_handlers_router, mock_callbacks_router):
     # вызов функции main
     await main()
 
@@ -12,5 +12,5 @@ async def test_main(mock_bot, mock_dispatcher, mock_setup_logger, mock_set_my_co
 
     #TODO - техдолг: доделать вызовы функций
     # mock_dispatcher.include_router.assert_awaited_once_with(mock_handlers_router, mock_callbacks_router)
-    # mock_bot.set_my_commands.assert_awaited_once()
+    # mock_set_my_commands.assert_awaited_once_with(mock_bot)
     # mock_setup_logger.assert_awaited_once()
