768e03925dc4d503602b57b674a25ed7a3005b1c
alinavoronina <alya.voronina.0600@mail.ru>
2025-05-05 15:23:47 +0300

    Описали клавиатуру "Выберите роль"

--- handlers/keyboard.py
+++ handlers/keyboard.py
@@ -1,4 +1,4 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup,  InlineKeyboardMarkup, InlineKeyboardButton
 
 def get_main_keyboard():
     keyboard = ReplyKeyboardMarkup(
@@ -9,3 +9,21 @@ def get_main_keyboard():
         one_time_keyboard=False  # Клавиатура остаётся после нажатия (можно сделать True, чтобы исчезала)
     )
     return keyboard
+
+# Inline-кнопка "Далее"
+# Inline-кнопка "Слушатель"
+# Inline-кнопка "Преподаватель"
+
+button_continue = InlineKeyboardButton(text="Далее", callback_data="button_continue")
+button_tutor = InlineKeyboardButton(text="Слушатель", callback_data="button_tutor")
+button_student = InlineKeyboardButton(text="Преподаватель", callback_data="button_student")
+
+# Inline-клавиатура "Продолжить"
+# Inline-клавиатура "Выберите роль"
+
+keyboard_continue = InlineKeyboardMarkup(inline_keyboard=[
+        [button_continue, ]
+    ])
+keyboard_continue = InlineKeyboardMarkup(inline_keyboard=[
+        [button_student, button_tutor]
+    ])
