fdbbe1267a9fba92de1645e0e2e8450d5a6a1364
IvanovaVictoria7 <ivanovavi1111@mail.ru>
2025-05-12 16:13:47 +0300

    описала клавиатуру "Выберите роль"

--- handlers/keyboard.py
+++ handlers/keyboard.py
@@ -1,5 +1,6 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup,InlineKeyboardMarkup, InlineKeyboardButton
 
+
 def get_main_keyboard():
     keyboard = ReplyKeyboardMarkup(
         keyboard=[
@@ -9,17 +10,24 @@ def get_main_keyboard():
         one_time_keyboard=False  # Клавиатура остаётся после нажатия (можно сделать True, чтобы исчезала)
     )
     return keyboard
-#Inline-кнопка "Далее"
-#Inline-кнопка "Слушатель"
-#Inline-кнопка "Преподаватель"
-button_continue = InlineKeyboardButton(text="Далее", callback_data="button_continue")
-button_tutor = InlineKeyboardButton(text="Слушатель", callback_data="button_tutor")
-button_student = InlineKeyboardButton(text="Преподаватель", callback_data="button_student")
-#Inline-клавиатура "Продолжить"
-#Inline-клавиатура "Выберите роль"
-keyboard_continue = InlineKeyboardMarkup(inline_keyboard=[
-        [button_continue, ]
-    ])
-keyboard_start = InlineKeyboardMarkup(inline_keyboard=[
-        [button_student, button_tutor]
-    ])
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+
+
+def get_continue_keyboard():
+    button_continue = InlineKeyboardButton(text="Далее", callback_data="button_continue")
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [button_continue]
+        ]
+    )
+    return keyboard
+
+def get_role_selection_keyboard():
+    button_tutor = InlineKeyboardButton(text="Преподаватель", callback_data="button_tutor")
+    button_student = InlineKeyboardButton(text="Ученик", callback_data="button_student")
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [button_student, button_tutor]
+        ]
+    )
+    return keyboard
