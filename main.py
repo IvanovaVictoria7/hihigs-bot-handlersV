03a7737ac58cc10f4d155f884b6c52138d8e3535
di-shinkareva <shinkareva.dr@mail.ru>
2025-06-23 18:08:17 +0300

    исправила мэйн

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
-# version1.0.0
+# version 1.0.0
 import asyncio
 import logging
+from os import name
+
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from handlers import router as handlers_router
 from handlers.callbacks import router as callbacks_router
 from handlers.bot_commands import set_my_commands
-from utils import setup_logger
+from utils.logging import setup_logger  # ✅ импорт логгера
 from db import async_create_table
 
 
 async def main():
+    setup_logger()  # ✅ логгер нужно вызывать до запуска
 
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
 
-    # Здесь функция для вызова хендлеров из handlers.py
+    # Подключаем все хендлеры
     dp.include_router(handlers_router)
     dp.include_router(callbacks_router)
 
-    # Здесь вызов меню с командами бота
+    # Устанавливаем команды бота
     await set_my_commands(bot)
 
-    setup_logger()
-
+    # Запускаем бота
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    asyncio.run(async_create_table())
+
+if name == "__main__":  # ✅ исправлено с name → name
+    asyncio.run(async_create_table())  # создаём таблицы в БД
     asyncio.run(main())
     logging.info("Бот остановлен")
-
-
