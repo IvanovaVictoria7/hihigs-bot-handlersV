fd85a2c32190a4a5f10be56cc4e312498546e162
IvanovaVictoria7 <ivanovavi1111@mail.ru>
2025-06-24 20:32:10 +0300

    добавила данные

--- db/engine.py
+++ db/engine.py
@@ -1,15 +1,18 @@
-__all__ = [
+all = [
     "async_create_table",
-    "async_sessionmaker",
+    "async_session"
 ]
 
-from sqlalchemy.ext.asyncio import async_sessionmaker
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from .models import Base
 
-engine = create_async_engine(url="sqlite+aiosqlite:///instance/sqlite.db", echo=True)
+# Создаём асинхронный движок (SQLite через aiosqlite)
+engine = create_async_engine("sqlite+aiosqlite:///instance/sqlite.db", echo=False)
+
+# Фабрика сессий
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
-async def async_create_table() -> None:
+# Создание таблиц
+async def async_create_table():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
