45e6092b7e54030dcf806a4834bd5f0f3857310a
IvanovaVictoria7 <ivanovavi1111@mail.ru>
2025-06-24 20:27:18 +0300

    добавила доп импорт

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,9 +1,15 @@
-# Файл __init__.py.py позволяет обращаться к папке как к модулю
-# и импортировать из него содержимое
-
-from .handlers import router
+from .handlers import router as router
+from .callbacks import router as callbacks_router
 from .bot_commands import set_my_commands
 from .logging import setup_logger
 from .keyboard import get_main_keyboard
+
+__all__ = [
+    "router",
+    "set_my_commands",
+    "setup_logger",
+    "get_main_keyboard",
+    "callbacks_router"
+]
 
 
